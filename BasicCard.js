2f3d7d776aa8a1765ed0aab4123fdd13fe7cfbad
Don Bedwell <timetourst@gmail.com>
2017-07-17 19:59:32 -0400

    Update BasicCard.js

--- BasicCard.js
+++ BasicCard.js
@@ -1,1 +1,9 @@
 // basic flashcard generator
+var firstPresident = new BasicCard(
+    "Who was the first president of the United States?", "George Washington");
+
+// "Who was the first president of the United States?"
+console.log(firstPresident.front); 
+
+// "George Washington"
+console.log(firstPresident.back); 
