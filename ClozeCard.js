9a0d76781289c31a575ec9101d4ed718468fff91
Don Bedwell <timetourst@gmail.com>
2017-07-17 20:00:01 -0400

    Update ClozeCard.js

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,1 +1,15 @@
 // cloze card generator
+var firstPresidentCloze = new ClozeCard(
+    "George Washington was the first president of the United States.", "George Washington");
+
+// "George Washington"
+console.log(firstPresidentCloze.cloze); 
+
+// " ... was the first president of the United States.
+console.log(firstPresidentCloze.partial); "
+
+// "George Washington was the first president of the United States.
+console.log(firstPresidentCloze.fullText): "
+
+// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+var brokenCloze = new ClozeCard("This doesn't work", "oops");
